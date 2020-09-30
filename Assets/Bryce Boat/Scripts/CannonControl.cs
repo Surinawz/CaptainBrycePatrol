bc08f908e95a0e9c6424607862d4666b8a669396
Zach_Surina <47762558+UnderWalk@users.noreply.github.com>
2020-09-30 14:07:47 -0400

    Initial Commit
    Version 0.0.81

---
+++ Assets/Bryce Boat/Scripts/CannonControl.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CannonControl : MonoBehaviour
+{
+#region Assign Variable
+    #region Cannon Attributes
+        [SerializeField] private float cannonAimSpeed; // how fast the cannon turns
+        [SerializeField] private ParticleSystem cannonFire; // TO DO: Make an explosion on firing
+        private Quaternion currentAngle; // stores current angle for lerping
+        [SerializeField] private float lerpSpeed; // Sets lerp speed for rotating cannon
+    #endregion
+    #region Cannon Ball Attributes
+        [SerializeField] GameObject cannonBall; // For assigning Cannon Ball Prefab for Instantiation
+        Rigidbody cannonBallBody; // Assigns rigid body for cannon Balls after instantiation
+        [SerializeField] public float firePower; // Cannon Ball Firing Speed
+        public Transform shotPos; // Stores Position and Angle when Instantiating Cannon Ball
+    #endregion
+    #region Controller Throw Variables
+        private float yThrow; //Right Thumbstick up and down
+        private float xThrow; //Right Thumbstick left and right
+        private float ovrYThrow; //Oculus Specific Right Thumbstick up and down
+        private float ovrXThrow; //Oculus Specific Right Thumbstick left and right
+        //private float cannonThrow; // Fire 1 button for testing in editor
+        private float ovrCannonThrow; // Oculus Specific Right Index Trigger
+        private Vector2 rightThumbStick;
+    #endregion
+#endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Instantiating a value for Testing
+        currentAngle = transform.localRotation;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    #region Cannon Controls
+        FireCannon();
+        RotateCannon();
+    #endregion
+    }
+
+    private void RotateCannon()
+    {
+        #region //Get Current Status of Cannon Angle and Controller Input
+        currentAngle = transform.localRotation;
+        yThrow += Input.GetAxis("Mouse Y") * cannonAimSpeed;
+        xThrow += Input.GetAxis("Mouse X") * cannonAimSpeed;
+        #endregion
+
+        #region //Limit Cannon Rotation
+        if (currentAngle.eulerAngles.y > 30 && currentAngle.eulerAngles.y < 100)
+        {
+            xThrow = -cannonAimSpeed;
+        }
+        if (currentAngle.eulerAngles.y < 330 && currentAngle.eulerAngles.y > 200)
+        {
+            xThrow = cannonAimSpeed;
+        }
+        if (currentAngle.eulerAngles.x < 330)
+        {
+            yThrow = -cannonAimSpeed;
+        }
+        #endregion
+
+        #region // Update Rotation Destination and Lerp
+        Quaternion updatedRotation = Quaternion.Euler(yThrow, xThrow, 0f);
+        transform.localRotation = Quaternion.Lerp(currentAngle, updatedRotation, Time.deltaTime * lerpSpeed);
+        #endregion
+
+    }
+
+    private void FireCannon()
+    {
+        #region // Count Existing Projectives and Suppress Fire at 30
+        float cannonCallCount = GameObject.FindGameObjectsWithTag("Cannonball").Length;
+        if (cannonCallCount > 30)
+        {
+            return;
+        }
+        #endregion
+        #region // Fire Projectile
+        else 
+        {
+            shotPos.rotation = transform.rotation;
+            ovrCannonThrow = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
+            //cannonThrow = Input.GetAxis("Fire1");
+            print(shotPos.position);
+            if (ovrCannonThrow > .5)
+            {
+                GameObject cannonBallCopy = Instantiate(cannonBall, shotPos.position, shotPos.rotation) as GameObject;
+                cannonBallBody = cannonBallCopy.GetComponent<Rigidbody>();
+                cannonBallBody.AddForce(transform.forward * firePower);
+            }
+        }
+        #endregion
+    }
+}
+
+
+
---
+++ Assets/Bryce Boat/Scripts/PlayerControl.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerControl : MonoBehaviour
+{
+
+    #region Assign Variables
+
+    #region Boat Movement Attributes
+    [SerializeField] private float boatSpeed=8000; // forward thrust
+    [SerializeField] private float turnSpeed=60; // speed of rotation in turns
+    [SerializeField] private float lift=5000; // Power of lift and drop
+    [SerializeField] private float rotationFromFriction=5; // angle of centrifugal force when turning
+    [SerializeField] private float planeForce=-2; // rising angle of bow when moving forward
+    [SerializeField] private float liftForce=20; // rising angle of bow when lifting
+    private Rigidbody boatRigidbody; // assigns rigidbody for boat
+    private float turning = 0; // instantiate float for turn calculation later
+#endregion
+
+    #region Level Objects 
+    [SerializeField] private Plane water;
+#endregion
+
+    #region Controller Throw Values
+    private float horizontalThrow;
+    private float verticalThrow;
+    private float liftThrow;
+    private float dropThrow;
+    private float ovrLiftThrow;
+    private float ovrDropThrow;
+    #endregion
+
+#endregion  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    #region Assign Components
+            boatRigidbody = GetComponent<Rigidbody>();
+    #endregion
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    #region Method for Boat Movement
+        ProcessTranslation();
+        ProcessRotation();
+        ProcessLift();
+        ProcessDrop();
+        ProcessFloat();
+#endregion
+    }
+
+   
+
+    private void ProcessFloat()
+    {
+    #region Provide lift force to bounce at water level
+            if (transform.position.y < 2)
+            {
+                boatRigidbody.AddRelativeForce(Vector3.up*lift*Time.deltaTime);
+            }
+    #endregion
+    }
+
+    private void ProcessLift()
+    {
+    #region Assign Controller Throw Values
+            liftThrow = Input.GetAxis("Jump");
+            ovrLiftThrow = OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger);
+    #endregion
+    #region Activate Lift with a cap of Y at 150
+            if ((liftThrow>0||ovrLiftThrow>0)&&transform.localPosition.y<150)
+            {
+                boatRigidbody.AddRelativeForce(Vector3.up * lift * Time.deltaTime);
+             };
+    #endregion
+    }
+
+    private void ProcessDrop()
+    {
+    #region Assign Controller Throw Values
+            dropThrow = Input.GetAxis("Fire1");
+            ovrDropThrow = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger);
+    #endregion
+    #region Activate Drop with Floor of Y at 10
+            if ((dropThrow > 0 || ovrDropThrow > 0) && transform.localPosition.y > 10)
+            {
+                boatRigidbody.AddRelativeForce(Vector3.down * lift * Time.deltaTime);
+            };
+    #endregion
+    }
+
+    private void ProcessTranslation()
+    {
+    #region Assign Controller Throw Values
+            verticalThrow = Input.GetAxis("Vertical");
+    #endregion
+
+    #region Activate Thrust
+        #region Activate Forward Thrust
+            if (verticalThrow > 0)
+            {
+                boatRigidbody.AddRelativeForce(Vector3.forward * verticalThrow * boatSpeed*Time.deltaTime);
+            }
+        #endregion
+        #region Activate Aft Thrust at half power
+            else if (verticalThrow < 0)
+            {
+                boatRigidbody.AddRelativeForce(Vector3.forward * verticalThrow * boatSpeed* .5f*Time.deltaTime);
+            }
+        #endregion
+    #endregion  
+    }
+
+    private void ProcessRotation()
+    {
+    #region Assign Controller Throw Values
+            horizontalThrow = Input.GetAxis("Horizontal");
+    #endregion
+    #region Assign All Rotation Parameters
+            turning += (horizontalThrow * turnSpeed * Time.deltaTime); // Left and Right Turn 
+                float turnForce = transform.localRotation.z + (horizontalThrow * rotationFromFriction); // Z Rotation to simulate Centrifugal Force
+                liftForce = -boatRigidbody.velocity.y; // Modifier for Bow Angle based on Lift or Drop
+                float ridePlane = transform.localRotation.x + (verticalThrow * planeForce) + (liftForce); //Modifier for Bow Angle with Forward Velocity with Lift/Drop
+    #endregion
+    #region Update Rotation
+            transform.localRotation = Quaternion.Euler(ridePlane, turning, turnForce);
+    #endregion  
+    }
+}
